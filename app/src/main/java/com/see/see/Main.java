675e6c364119663d6d66ba8fca3d3af84f715ee3
andrescordova@outlook.com <andrescordova@outlook.com>
2015-03-16 11:35:07 -0400

    Initial commit

---
+++ app/src/main/java/com/see/see/Main.java
@@ -0,0 +1,200 @@
+package com.see.see;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.SurfaceHolder;
+
+import net.majorkernelpanic.streaming.Session;
+import net.majorkernelpanic.streaming.SessionBuilder;
+import net.majorkernelpanic.streaming.audio.AudioQuality;
+import net.majorkernelpanic.streaming.gl.SurfaceView;
+import net.majorkernelpanic.streaming.rtsp.RtspClient;
+import net.majorkernelpanic.streaming.video.VideoQuality;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main extends Activity implements RtspClient.Callback, Session.Callback, SurfaceHolder.Callback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        // getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_main);
+
+        mSurfaceView = (SurfaceView) findViewById(R.id.surface);
+
+        mSurfaceView.getHolder().addCallback(this);
+
+        // Initialize RTSP client
+        initRtspClient();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+
+
+    // log tag
+    public final static String TAG = Main.class.getSimpleName();
+
+    // surfaceview
+    private static SurfaceView mSurfaceView;
+
+    // Rtsp session
+    private Session mSession;
+    private static RtspClient mClient;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        toggleStreaming();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        toggleStreaming();
+    }
+
+    private void initRtspClient() {
+        // Configures the SessionBuilder
+        mSession = SessionBuilder.getInstance()
+                .setContext(getApplicationContext())
+                .setAudioEncoder(SessionBuilder.AUDIO_NONE)
+                .setAudioQuality(new AudioQuality(8000, 16000))
+                .setVideoEncoder(SessionBuilder.VIDEO_H264)
+                .setSurfaceView(mSurfaceView).setPreviewOrientation(0)
+                .setCallback(this).build();
+
+        // Configures the RTSP client
+        mClient = new RtspClient();
+        mClient.setSession(mSession);
+        mClient.setCallback(this);
+        mSurfaceView.setAspectRatioMode(SurfaceView.ASPECT_RATIO_PREVIEW);
+        String ip, port, path;
+
+        // We parse the URI written in the Editext
+        Pattern uri = Pattern.compile("rtsp://(.+):(\\d+)/(.+)");
+        Matcher m = uri.matcher(AppConfig.STREAM_URL);
+        m.find();
+        ip = m.group(1);
+        port = m.group(2);
+        path = m.group(3);
+
+        mClient.setCredentials(AppConfig.PUBLISHER_USERNAME,
+                AppConfig.PUBLISHER_PASSWORD);
+        mClient.setServerAddress(ip, Integer.parseInt(port));
+        mClient.setStreamPath("/" + path);
+    }
+
+    private void toggleStreaming() {
+        if (!mClient.isStreaming()) {
+            // Start camera preview
+            mSession.startPreview();
+
+            // Start video stream
+            mClient.startStream();
+        } else {
+            // already streaming, stop streaming
+            // stop camera preview
+            mSession.stopPreview();
+
+            // stop streaming
+            mClient.stopStream();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mClient.release();
+        mSession.release();
+        mSurfaceView.getHolder().removeCallback(this);
+    }
+
+
+
+
+
+
+    @Override
+    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder arg0) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void onBitrateUpdate(long bitrate) {
+
+    }
+
+    @Override
+    public void onSessionError(int reason, int streamType, Exception e) {
+
+    }
+
+    @Override
+    public void onPreviewStarted() {
+
+    }
+
+    @Override
+    public void onSessionConfigured() {
+
+    }
+
+    @Override
+    public void onSessionStarted() {
+
+    }
+
+    @Override
+    public void onSessionStopped() {
+
+    }
+
+    @Override
+    public void onRtspUpdate(int message, Exception exception) {
+
+    }
+}
