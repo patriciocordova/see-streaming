19f2037efdc372d19f3b3a6b19fe37a77ff42c9e
harshab85 <harshab85@gmail.com>
2015-04-09 11:09:10 -0400

    Fix in poll

--- app/src/main/java/com/stream/service/request/AbstractRequest.java
+++ app/src/main/java/com/stream/service/request/AbstractRequest.java
@@ -7,6 +7,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -71,6 +72,51 @@ public abstract class AbstractRequest {
     }
 
     protected JSONObject executePost(HttpPost post){
+        HttpResponse response = null;
+        ByteArrayOutputStream out = null;
+        try {
+            response = httpClient.execute(post);
+            StatusLine statusLine = response.getStatusLine();
+            if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+                out = new ByteArrayOutputStream();
+                response.getEntity().writeTo(out);
+                String responseString = out.toString();
+                JSONObject object = new JSONObject(responseString);
+                return object;
+            }
+        }
+        catch (Exception e){
+            // ignore
+        }
+        finally{
+
+            try {
+                if(response != null){
+                    HttpEntity entity = response.getEntity();
+                    if(entity != null) {
+                        InputStream stream = entity.getContent();
+                        if (stream != null) {
+                            stream.close();
+                        }
+                    }
+                }
+
+                if(out != null){
+                    out.flush();
+                    out.close();
+                }
+
+            }
+            catch (Exception e){
+                // Ignore
+            }
+        }
+
+        return null;
+    }
+
+
+    protected JSONObject executeDelete(HttpDelete post){
         HttpResponse response = null;
         ByteArrayOutputStream out = null;
         try {
